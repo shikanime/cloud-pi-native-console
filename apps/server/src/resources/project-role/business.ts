b7bb1502b12acf404f52476804d696a6c47e5c4f
William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>
2026-01-30 09:57:14 +0100

    feat: add OIDC group to project groups
    Signed-off-by: William Phetsinorath <william.phetsinorath-open@interieur.gouv.fr>

--- apps/server/src/resources/project-role/business.ts
+++ apps/server/src/resources/project-role/business.ts
@@ -6,15 +6,21 @@ import {
   listRoles as listRolesQuery,
   updateRole,
 } from '@/resources/queries-index.js'
-import { BadRequest400 } from '@/utils/errors.js'
+import { BadRequest400, Forbidden403 } from '@/utils/errors.js'
 import prisma from '@/prisma.js'
 
 export async function listRoles(projectId: Project['id']) {
-  return listRolesQuery(projectId)
-    .then(roles => roles.map(role => ({ ...role, permissions: role.permissions.toString() })))
+  const project = await prisma.project.findUnique({ where: { id: projectId }, select: { slug: true } })
+  const roles = await listRolesQuery(projectId)
+  return roles.map(role => ({
+    ...role,
+    permissions: role.permissions.toString(),
+    oidcGroup: project?.slug ? role.oidcGroup?.replace(new RegExp(`^/project-${project.slug}/`), '') : role.oidcGroup,
+  }))
 }
 
 export async function patchRoles(projectId: Project['id'], roles: typeof projectRoleContract.patchProjectRoles.body._type) {
+  const project = await prisma.project.findUnique({ where: { id: projectId }, select: { slug: true } })
   const dbRoles = await listRoles(projectId)
   const positionsAvailable: number[] = []
 
@@ -25,11 +31,21 @@ export async function patchRoles(projectId: Project['id'], roles: typeof project
       if (typeof matchingRole?.position !== 'undefined' && !positionsAvailable.includes(matchingRole.position)) {
         positionsAvailable.push(matchingRole.position)
       }
+      if (dbRole.type === 'system') {
+        throw new Forbidden403('Ce rôle système ne peut pas être renommé')
+      }
+
+      let oidcGroup = matchingRole?.oidcGroup ?? dbRole.oidcGroup
+      if (oidcGroup && project?.slug && !oidcGroup.startsWith(`/project-${project.slug}/`)) {
+        oidcGroup = `/project-${project.slug}/${oidcGroup}`
+      }
+
       return {
         id: matchingRole?.id ?? dbRole.id,
         name: matchingRole?.name ?? dbRole.name,
         permissions: matchingRole?.permissions ? BigInt(matchingRole?.permissions) : BigInt(dbRole.permissions),
         position: matchingRole?.position ?? dbRole.position,
+        oidcGroup,
       }
     })
   if (positionsAvailable.length && positionsAvailable.length !== dbRoles.length) return new BadRequest400('Les numéros de position des rôles sont incohérentes')
@@ -41,11 +57,17 @@ export async function patchRoles(projectId: Project['id'], roles: typeof project
 }
 
 export async function createRole(projectId: Project['id'], role: typeof projectRoleContract.createProjectRole.body._type) {
+  const project = await prisma.project.findUnique({ where: { id: projectId }, select: { slug: true } })
   const dbMaxPosRole = (await prisma.projectRole.findFirst({
     where: { projectId },
     orderBy: { position: 'desc' },
     select: { position: true },
   }))?.position ?? -1
+
+  let oidcGroup = role.oidcGroup
+  if (oidcGroup && project?.slug && !oidcGroup.startsWith(`/project-${project.slug}/`)) {
+    oidcGroup = `/project-${project.slug}/${oidcGroup}`
+  }
 
   await prisma.projectRole.create({
     data: {
@@ -53,6 +75,7 @@ export async function createRole(projectId: Project['id'], role: typeof projectR
       projectId,
       position: dbMaxPosRole + 1,
       permissions: BigInt(role.permissions),
+      oidcGroup,
     },
   })
 
